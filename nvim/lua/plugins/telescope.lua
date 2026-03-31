cbd0f7b297a3316b6b1bfc43d64eaa3ff4fa2ce9
z <z@earlysloth.com>
2026-03-31 13:03:57 +0900

    Add telescope.nvim for fuzzy file and symbol search

---
+++ nvim/lua/plugins/telescope.lua
@@ -0,0 +1,43 @@
+return {
+  {
+    "nvim-telescope/telescope.nvim",
+    branch = "0.1.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      {
+        "nvim-telescope/telescope-fzf-native.nvim",
+        build = "make",
+      },
+    },
+    cmd = "Telescope",
+    keys = {
+      { "<leader>ff", "<cmd>Telescope find_files<CR>", desc = "Find files" },
+      { "<leader>fg", "<cmd>Telescope live_grep<CR>", desc = "Live grep" },
+      { "<leader>fb", "<cmd>Telescope buffers<CR>", desc = "Buffers" },
+      { "<leader>fs", "<cmd>Telescope lsp_document_symbols<CR>", desc = "Document symbols" },
+      { "<leader>fS", "<cmd>Telescope lsp_workspace_symbols<CR>", desc = "Workspace symbols" },
+      { "<leader>fd", "<cmd>Telescope diagnostics<CR>", desc = "Diagnostics" },
+      { "<leader>fr", "<cmd>Telescope resume<CR>", desc = "Resume last search" },
+    },
+    config = function()
+      local telescope = require("telescope")
+
+      telescope.setup({
+        defaults = {
+          sorting_strategy = "ascending",
+          layout_config = {
+            prompt_position = "top",
+          },
+          file_ignore_patterns = { "%.git/", "node_modules/", "vendor/" },
+        },
+        pickers = {
+          find_files = {
+            hidden = true,
+          },
+        },
+      })
+
+      telescope.load_extension("fzf")
+    end,
+  },
+}
