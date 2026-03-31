1b1a4297ce4f4fb593e891c7d331ff968c08e1b2
z <z@earlysloth.com>
2026-03-31 14:37:44 +0900

    Add jq formatting for JSON files in Neovim

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -1,3 +1,10 @@
 vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter", "CursorHold" }, {
   command = "checktime",
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = "json",
+  callback = function()
+    vim.bo.formatprg = "jq ."
+  end,
+})
