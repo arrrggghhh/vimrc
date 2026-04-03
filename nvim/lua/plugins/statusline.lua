96519f702f9493b89a45794237a75b0030f4c05a
z <z@earlysloth.com>
2026-04-03 14:49:47 +0900

    Show relative paths in inactive statusline

--- nvim/lua/plugins/statusline.lua
+++ nvim/lua/plugins/statusline.lua
@@ -16,5 +16,13 @@ return {
       lualine_y = { "progress" },
       lualine_z = { "location" },
     },
+    inactive_sections = {
+      lualine_a = {},
+      lualine_b = {},
+      lualine_c = { { "filename", path = 1, shorting_target = 0 } },
+      lualine_x = { "location" },
+      lualine_y = {},
+      lualine_z = {},
+    },
   },
 }
