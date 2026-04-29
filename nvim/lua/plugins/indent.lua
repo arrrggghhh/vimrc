b83900caa52c50789060cf826c221f6ed8d99462
z <z@earlysloth.com>
2026-04-29 14:44:12 +0900

    Hide the first indent guide

--- nvim/lua/plugins/indent.lua
+++ nvim/lua/plugins/indent.lua
@@ -19,6 +19,9 @@ return {
   config = function(_, opts)
     local hooks = require("ibl.hooks")
 
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_space_indent_level)
+    hooks.register(hooks.type.WHITESPACE, hooks.builtin.hide_first_tab_indent_level)
+
     hooks.register(hooks.type.HIGHLIGHT_SETUP, function()
       vim.api.nvim_set_hl(0, "IblIndent", { fg = "#4f4f63" })
       vim.api.nvim_set_hl(0, "IblScope", { fg = "#60607a" })
